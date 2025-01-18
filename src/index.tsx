e0ecb68b4ebbbd12e8a728cbcb348657a4dd2360
masotnikov <masotnikov11@yandex.ru>
2025-01-18 14:12:48 +0300

    feat(components): added initial component structure

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from "./app/App";
+import {BrowserRouter} from "react-router-dom";
+
+console.log('React application starts rendering...');
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+    throw new Error('Root element not found');
+}
+const root = createRoot(rootElement);
+
+root.render(
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+);
