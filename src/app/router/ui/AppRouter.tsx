fa947d5af924a32670f1a333e857d1fed0923aee
masotnikov <masotnikov11@yandex.ru>
2025-01-19 13:10:17 +0300

    feat(routing): added AppRouter

---
+++ src/app/router/ui/AppRouter.tsx
@@ -0,0 +1,32 @@
+import {FC, Suspense} from "react";
+import {Route, Routes} from "react-router-dom";
+import {routeConfig} from "@/app/router/config/routeConfig";
+
+
+interface AppRouterProps {
+  className?: string;
+}
+
+export const AppRouter: FC<AppRouterProps> = (props) => {
+
+  return (
+    <Suspense fallback={<h1>...Loading</h1>}>
+      <Routes>
+        {routeConfig.map(({path, element}) => (
+          <Route
+            key={path}
+            path={path}
+            element={(
+              <div className="container">
+                {element}
+              </div>
+            )}
+          >
+          </Route>
+        ))}
+      </Routes>
+    </Suspense>
+
+  );
+};
+
